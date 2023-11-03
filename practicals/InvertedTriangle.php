5409c695fc86c57fa4ea9ea5c275a5c5aa2647f7
ArpitAgrawal001 <agrawa271@gmail.com>
2023-11-04 03:01:49 +0530

    Update InvertedTriangle program for readability and maintainability

--- practicals/InvertedTriangle.php
+++ practicals/InvertedTriangle.php
@@ -1,10 +1,23 @@
 <?php
-$height = 5; // Change the height of the inverted triangle as needed
 
-for ($i = $height; $i >= 1; $i--) {
-    for ($j = 1; $j <= $i; $j++) {
-        echo "* ";
+function printInvertedTriangle($height) {
+    if ($height <= 0) {
+        echo "Height must be a positive integer.";
+        return;
     }
-    echo "\n";
+
+    for ($i = $height; $i >= 1; $i--) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "* ";
+        }
+        echo PHP_EOL;
+    }
 }
+
+// Define the height of the inverted triangle
+$triangleHeight = 5; // Change this value to adjust the size of the inverted triangle
+
+// Print the inverted triangle
+printInvertedTriangle($triangleHeight);
+echo "\n";
 ?>
