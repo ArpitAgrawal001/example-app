7eb843c29b2560f0b4d8fc2ef20532eb249af37a
ArpitAgrawal001 <agrawa271@gmail.com>
2023-11-25 14:42:29 +0530

    Move css to main.css

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -23,6 +23,8 @@
         <ul>
             <li>Playlist 1</li>
             <li>Playlist 2</li>
+            <li>Playlist 3</li>
+
             <!-- Add more playlists as needed -->
         </ul>
     </sidebar>
